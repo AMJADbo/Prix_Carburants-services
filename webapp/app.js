16d4047f99257f865df081177a09018e187475f2
peigney-anais <anais.peigney@groupe-esigelec.org>
2026-03-17 09:51:59 +0100

    Added legend on the map and enhanced style

--- webapp/app.js
+++ webapp/app.js
@@ -146,6 +146,33 @@ function renderStationsOnMap(stations) {
   setTimeout(() => {
     map.invalidateSize();
   }, 100);
+
+  // Ajoute une légende fixe
+  let legend = document.getElementById('map-legend');
+
+  if (!legend) {
+    legend = document.createElement('div');
+    legend.id = 'map-legend';
+    legend.className = 'map-legend'; // La classe CSS fera tout le travail
+
+    legend.innerHTML = `
+      <div class="legend-item" id="legend-user">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1d6bff" class="bi bi-geo-alt" viewBox="0 0 16 16">
+          <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+          <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+        </svg>
+        <span id="legend-user-label">Votre position</span>
+      </div>
+      <div class="legend-item" id="legend-station">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#00c950" class="bi bi-circle-fill" viewBox="0 0 16 16">
+          <circle cx="8" cy="8" r="8"/>
+        </svg>
+        <span id="legend-station-label">Stations essence</span>
+      </div>
+    `;
+
+    document.getElementById('map').appendChild(legend);
+  }
 }
 
 // Ajoute un observer pour recalculer la carte quand #map devient visible
